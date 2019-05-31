8b63e00b173dfaf90e75a1ae6eafe8b9aee83a67
hl0516 <765143212@qq.com>
2019-05-31 20:22:38 +0800

    初步框架

---
+++ src/main/java/com/bigeng/invoicing/InvoicingApplication.java
@@ -0,0 +1,13 @@
+package com.bigeng.invoicing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoicingApplication.class, args);
+    }
+
+}
