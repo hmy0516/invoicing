1833aeee46e4464b739d361a2815a5c84944c1a1
hl0516 <765143212@qq.com>
2019-06-03 19:46:57 +0800

    操作人员

--- src/main/java/com/bigeng/invoicing/mapper/UsersMapper.java
+++ src/main/java/com/bigeng/invoicing/mapper/UsersMapper.java
@@ -1,4 +1,10 @@
 package com.bigeng.invoicing.mapper;
 
+import com.bigeng.invoicing.pojo.system.Users;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface UsersMapper {
+
+    Users queryUser(String id);
 }
