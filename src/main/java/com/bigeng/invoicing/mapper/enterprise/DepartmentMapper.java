7b3722975fdbc0a7d39fad1fffc8fcbdeb41f08e
huchengjin <1779956808@qq.com>
2019-06-04 21:31:55 +0800

    统一消息类

--- src/main/java/com/bigeng/invoicing/mapper/enterprise/DepartmentMapper.java
+++ src/main/java/com/bigeng/invoicing/mapper/enterprise/DepartmentMapper.java
@@ -1,8 +1,6 @@
 package com.bigeng.invoicing.mapper.enterprise;
 
-import com.bigeng.invoicing.pojo.enterprise.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
