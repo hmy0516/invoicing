ee2e32c0e7275806d85f32e16ed142bbdfaead61
xiajinlong <952460738@qq.com>
2019-06-04 08:48:43 +0800

    部门管理

--- src/main/java/com/bigeng/invoicing/pojo/enterprise/Department.java
+++ src/main/java/com/bigeng/invoicing/pojo/enterprise/Department.java
@@ -1,4 +1,44 @@
 package com.bigeng.invoicing.pojo.enterprise;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="t_hr_department")
 public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String cdid;
+
+    private String cdepartment;
+    private  String ccomment;
+
+    public String getCdid() {
+        return cdid;
+    }
+
+    public void setCdid(String cdid) {
+        this.cdid = cdid;
+    }
+
+    public String getCdepartment() {
+        return cdepartment;
+    }
+
+    public void setCdepartment(String cdepartment) {
+        this.cdepartment = cdepartment;
+    }
+
+    public String getCcomment() {
+        return ccomment;
+    }
+
+    public void setCcomment(String ccomment) {
+        this.ccomment = ccomment;
+    }
 }
