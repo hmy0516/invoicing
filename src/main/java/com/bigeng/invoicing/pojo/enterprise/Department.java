c19e8459c3bc8c032b5e3c9dd7631f4d20dfe5d0
hl0516 <765143212@qq.com>
2019-06-04 09:26:30 +0800

    建包规范

--- src/main/java/com/bigeng/invoicing/pojo/enterprise/Department.java
+++ src/main/java/com/bigeng/invoicing/pojo/enterprise/Department.java
@@ -11,4 +11,43 @@ import javax.persistence.Table;
 @Entity
 @Table(name="t_hr_department")
 public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String cDid;
+
+    private String cDepartment;
+    private  String cComment;
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "cDid='" + cDid + '\'' +
+                ", cDepartment='" + cDepartment + '\'' +
+                ", cComment='" + cComment + '\'' +
+                '}';
+    }
+
+    public String getcDid() {
+        return cDid;
+    }
+
+    public void setcDid(String cDid) {
+        this.cDid = cDid;
+    }
+
+    public String getcDepartment() {
+        return cDepartment;
+    }
+
+    public void setcDepartment(String cDepartment) {
+        this.cDepartment = cDepartment;
+    }
+
+    public String getcComment() {
+        return cComment;
+    }
+
+    public void setcComment(String cComment) {
+        this.cComment = cComment;
+    }
 }
