28213352d57adb2129aab2e9c7a8be5f83bcf6e4
huchengjin <1779956808@qq.com>
2019-06-04 21:32:53 +0800

    统一消息类

--- src/main/java/com/bigeng/invoicing/mapper/enterprise/DepartmentMapper.java
+++
@@ -1,9 +0,0 @@
-package com.bigeng.invoicing.mapper.enterprise;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentMapper extends JpaRepository<Department,Long> {
-
-}
