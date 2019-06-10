405a620db2b630d526f7edc391a6528e37413b2d
hl0516 <765143212@qq.com>
2019-06-10 15:04:27 +0800

    login

--- src/main/java/com/bigeng/invoicing/controller/system/UsersController.java
+++ src/main/java/com/bigeng/invoicing/controller/system/UsersController.java
@@ -1,5 +1,7 @@
 package com.bigeng.invoicing.controller.system;
 
+import com.bigeng.invoicing.pojo.RespMsg;
+import com.bigeng.invoicing.pojo.system.LoginForm;
 import com.bigeng.invoicing.pojo.system.Users;
 import com.bigeng.invoicing.service.system.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,5 +17,15 @@ public class UsersController {
     //@RequestMapping(value = "/queryUser/{id}",method = RequestMethod.GET)
     public Users queryUser(@PathVariable("id") String id){
         return usersService.queryUser(id);
+    }
+
+    @PostMapping(value = "/login")
+    public RespMsg login(@RequestBody LoginForm loginForm){
+        System.out.println(loginForm.getUsername()+"="+loginForm.getPassword());
+        Users user = usersService.login(loginForm.getUsername(), loginForm.getPassword());
+        if(user!=null)
+            return RespMsg.ok("登录成功!!",user);
+        else
+            return RespMsg.error("登录失败!!!");
     }
 }
--- src/main/java/com/bigeng/invoicing/pojo/system/LoginForm.java
+++ src/main/java/com/bigeng/invoicing/pojo/system/LoginForm.java
@@ -1,4 +1,30 @@
 package com.bigeng.invoicing.pojo.system;
 
 public class LoginForm {
+    private String username;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "LoginForm{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
